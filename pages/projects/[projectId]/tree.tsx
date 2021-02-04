e85d9fbc66a84d4cfe28a2dbebea9c8a8122c518
wgbg <kamil.szela.dev@gmail.com>
2021-02-04 22:44:02 +0100

    add dynamic tree

--- pages/projects/[projectId]/tree.tsx
+++ pages/projects/[projectId]/tree.tsx
@@ -40,10 +40,14 @@ import DescriptionIcon from '@material-ui/icons/Description';
 import PermIdentityIcon from '@material-ui/icons/PermIdentity';
 import {useTranslation} from "react-i18next";
 import {useSmallNotify} from "../../../helpers";
+import CreateProjectTreeLeafDialog from "../../../components/dialogs/create-project-tree-leaf";
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import {ALink} from "../../../components/link";
 
 interface TreeParams {
   project: Schemas.Project,
-  projectTreeRoot: Schemas.Tree
+  selectedLeaf: Schemas.Tree
 }
 
 const breadcrumbs = [
@@ -82,14 +86,14 @@ const useStyles = makeStyles((theme) => ({
     height: '2px'
   },
   breadCrumbs: {
-    fontSize: "13px"
+    fontSize: "18px"
   },
   breadCrumbsHome: {
-    fontSize: "15px"
+    fontSize: "20px"
   },
   breadCrumbsContainer: {
     borderTop: "2px solid #c0c0c01a",
-    background: '#bfbfbf69',
+    borderBottom: "2px solid #c0c0c01a",
     padding: "4px 5px;",
     marginTop: "0px",
     paddingLeft: '20px'
@@ -136,14 +140,24 @@ const useStyles = makeStyles((theme) => ({
     fontSize: '13px',
     textAlign: 'center',
     color: 'darkgrey',
+  },
+  tabsContainer: {
+    flexGrow: 1
   }
 }));
+
+interface BreadCrumbEntry
+{
+  tree: Schemas.Tree;
+  type: 'home' | 'leaf';
+  id: number;
+}
 
 function ListItemLink(props) {
   return <ListItem button component="a" {...props} />;
 }
 
-function Tree(params: TreeParams) {
+function Tree(params: TreeParams, el?) {
   const {successMessage, apiResponse} = useSmallNotify();
 
   const {t} = useTranslation(['project-tree'])
@@ -152,13 +166,84 @@ function Tree(params: TreeParams) {
     project: params.project
   })
 
+  const [creatingFolder, setCreatingFolder] = useState<boolean>(false)
+  const [currentLeaf, setCurrentLeaf] = useState<Schemas.Tree>(params.selectedLeaf)
   const [treeLeaves, setTreeLeaves] = useState<Schemas.Tree[]>([])
+  const [treeBreadcrumbs, setTreeBreadcrumbs] = useState<BreadCrumbEntry[]>([])
+  const [treeLeavesCount, setTreeLeavesCount] = useState<number>(0)
+
+  const [folderRowsPerPage, setFolderRowsPerPage] = React.useState(10);
+  const [folderRowsPage, setFolderRowsPage] = React.useState(0)
+
+  const handleFoldersChangePage = (event, newPage) => {
+    setFolderRowsPage(newPage);
+
+    reloadFolders(newPage)
+  };
+
+  const handleFoldersChangeRowsPerPage = (event) => {
+    setFolderRowsPerPage(event.target.value);
+    setFolderRowsPage(0);
+
+    reloadFolders(0, event.target.value);
+  };
+
+  const goToLeaf = (leaf) => {
+    setCurrentLeaf(leaf);
+    setFolderRowsPage(0);
+    reloadFolders(0);
+
+  }
+
+  const reloadFolders = (page?:number, perPage?: number) => {
+    apiBackend.trees.getLeaves(
+      currentLeaf.id,
+      (typeof page !== undefined ? page : folderRowsPage) + 1,
+      typeof perPage !== undefined? perPage : folderRowsPerPage
+    ).then((res) => {
+      setTreeLeaves(res.data)
+      setTreeLeavesCount(res.meta.total)
+    }).then(() => {
+      apiBackend.trees.getRootPath(currentLeaf.id).then((res) => {
+        const newBreadCrumbs : Array<BreadCrumbEntry> = [];
+        let lId = 1;
+
+        for(var leaf of res.data) {
+          if(newBreadCrumbs.length == 0 ){
+            newBreadCrumbs.push({
+              type: 'home',
+              tree: leaf,
+              id: lId
+            })
+
+            lId++;
+
+            continue;
+          }
+
+          newBreadCrumbs.push({
+            tree: leaf,
+            type: 'leaf',
+            id: lId
+          });
+
+          lId++;
+        }
+
+        setTreeBreadcrumbs(newBreadCrumbs);
+      });
+    })
+  }
+
+
+
+  if(params.selectedLeaf.id !== currentLeaf.id){
+    goToLeaf(params.selectedLeaf);
+  }
 
   useEffect(() => {
-    apiBackend.trees.get(params.projectTreeRoot.id).then((res) => {
-      setTreeLeaves(res.data)
-    })
-  })
+    reloadFolders();
+  }, [currentLeaf])
 
   return (
     <Layout breadcrumbs={breadcrumbs} pageTitle={pageTitle} pageTitleEditable={false}>
@@ -167,7 +252,7 @@ function Tree(params: TreeParams) {
           <Grid item xs={2} className={classes.subContainer + ' ' + classes.managerMenuContainer}>
             <Box className={classes.managerMenu}>
             <List component="nav" aria-label="main mailbox folders">
-              <ListItem button>
+              <ListItem button onClick={() => setCreatingFolder(true)}>
                 <ListItemIcon>
                   <CreateNewFolderIcon />
                 </ListItemIcon>
@@ -183,29 +268,24 @@ function Tree(params: TreeParams) {
             </Box>
           </Grid>
           <Grid item xs={10} className={classes.subContainer}>
-            <Box className={classes.fileManagerRow}>
-              <FormControl className={classes.searchInput}>
-                <InputBase
-                  id="input-with-icon-adornment"
-                  fullWidth
-                  placeholder={t('search')}
-                  className={classes.searchInputText}
-                  startAdornment={
-                    <InputAdornment position="start">
-                      <SearchIcon />
-                    </InputAdornment>
-                  }
-                />
-              </FormControl>
-            </Box>
             <Box className={classes.fileManagerRow + ' ' + classes.breadCrumbsContainer}>
               <Breadcrumbs aria-label="breadcrumb" className={classes.breadCrumbs}>
-                <Link color="inherit" href="/" >
-                  <HomeIcon className={classes.breadCrumbsHome}/>
-                </Link>
-                <Link color="inherit" href="/" >
-                  Path
-                </Link>
+                {treeBreadcrumbs.map((row) => {
+                    if(row.type === 'home') {
+                      return (
+                        <ALink href={ `/projects/${params.project.id}/tree?leaf=${row.tree.id}`} key={row.id}>
+                          <HomeIcon className={classes.breadCrumbsHome}/>
+                        </ALink>
+                      )
+                    }
+
+                    return (
+                      <ALink href={ `/projects/${params.project.id}/tree?leaf=${row.tree.id}`} key={row.id}>
+                        {row.tree.title}
+                      </ALink>
+                    )
+                  }
+                )}
               </Breadcrumbs>
             </Box>
             <Box className={classes.fileManagerRow}>
@@ -215,15 +295,18 @@ function Tree(params: TreeParams) {
                     <TableRow>
                       <TableCell className={classes.folderRowCell}>Folder name</TableCell>
                       <TableCell align="center" className={classes.folderRowCell}>{t('leaves.elements')}</TableCell>
-                      <TableCell align="center" className={classes.folderRowCell}>{t('menu.createdAt')}</TableCell>
-                      <TableCell align="center" className={classes.folderRowCell}>{t('menu.actions')}</TableCell>
+                      <TableCell align="center" className={classes.folderRowCell}>{t('leaves.createdAt')}</TableCell>
+                      <TableCell align="center" className={classes.folderRowCell}>{t('leaves.actions')}</TableCell>
                     </TableRow>
                   </TableHead>
                   <TableBody>
                     {treeLeaves.map((row) => (
-                      <TableRow key={row.id}>
+                      <TableRow hover key={row.id}>
                         <TableCell component="th" scope="row" className={classes.folderRowTitle + ' ' + classes.folderRowCell}>
-                          <FolderIcon  className={classes.folderRowTitleIcon}/> <p>{row.title}</p>
+                          <FolderIcon  className={classes.folderRowTitleIcon}/>
+                          <ALink href={ `/projects/${params.project.id}/tree?leaf=${row.id}`}>
+                            <p>{row.title}</p>
+                          </ALink>
                         </TableCell>
                         <TableCell align="center" className={classes.folderRowCell}>-</TableCell>
                         <TableCell align="center" className={classes.folderRowCell}>-</TableCell>
@@ -233,23 +316,44 @@ function Tree(params: TreeParams) {
                   </TableBody>
                 </Table>
               </TableContainer>
+              <TablePagination
+                rowsPerPageOptions={[10, 25, 100]}
+                component="div"
+                count={treeLeavesCount}
+                rowsPerPage={folderRowsPerPage}
+                page={folderRowsPage}
+                onChangePage={handleFoldersChangePage}
+                onChangeRowsPerPage={handleFoldersChangeRowsPerPage}
+              />
             </Box>
           </Grid>
         </Grid>
       </Paper>
+      <CreateProjectTreeLeafDialog
+        opened={creatingFolder}
+        handleClose={() => setCreatingFolder(false)}
+        treeLeafId={currentLeaf.id}
+        onFolderCreated={reloadFolders}
+      />
     </Layout>
   )
 }
 
 Tree.getInitialProps = async ({query}) => {
-
   const projectId = query.projectId;
   const project = await apiBackend.project.get(projectId)
-  const projectTreeRoot =  await apiBackend.project.getTreeRoot(projectId)
+
+  let selectedLeaf;
+
+  if(query.leaf){
+    selectedLeaf =  await apiBackend.trees.get(query.leaf)
+  } else {
+    selectedLeaf =  await apiBackend.project.getTreeRoot(projectId)
+  }
 
   return {
     project: getApiResponseData<Schemas.Project>(project),
-    projectTreeRoot: getApiResponseData<Schemas.Tree>(projectTreeRoot)
+    selectedLeaf: getApiResponseData<Schemas.Tree>(selectedLeaf),
   }
 }
 
