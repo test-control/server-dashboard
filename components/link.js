8f83eec06be332418d263f39049985c5c52ee5ae
wgbg <kamil.szela.dev@gmail.com>
2021-02-04 22:43:19 +0100

    remove prefetch attribute from links

--- components/link.js
+++ components/link.js
@@ -3,7 +3,7 @@ import Button from "@material-ui/core/Button";
 
 export class BaseLink extends React.Component {
   render() {
-    return <Link href={this.props.href} as={this.props.hrefAs} prefetch>
+    return <Link href={this.props.href} as={this.props.hrefAs}>
       <a className={this.props.className}>
         {this.props.children}
       </a>
@@ -13,13 +13,13 @@ export class BaseLink extends React.Component {
 
 export class ALink extends React.Component {
   render() {
-    return <BaseLink href={this.props.href} as={this.props.hrefAs} className={this.props.className} prefetch>{this.props.children}</BaseLink>;
+    return <BaseLink href={this.props.href} as={this.props.hrefAs} className={this.props.className}>{this.props.children}</BaseLink>;
   }
 }
 
 export class ButtonLink extends React.Component {
   render() {
-    return <BaseLink href={this.props.href} as={this.props.hrefAs} prefetch className={this.props.className}>
+    return <BaseLink href={this.props.href} as={this.props.hrefAs} className={this.props.className}>
       <Button variant={this.props.buttonVariant} color={this.props.buttonColor} component='a' size={this.props.buttonSize}>
         {this.props.children}
       </Button>
